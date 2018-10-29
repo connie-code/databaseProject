ddfcb2a24525d26f120aa22f9a666d4cd96d84c2
connie-code <conniewu06@gmail.com>
2018-10-29 17:38:07 -0400

    'adding_all_project_files'

---
+++ app.js
@@ -0,0 +1,290 @@
+// Need to npm install --save express, mysql, ejs, and body-parser
+var express = require('express');
+var mysql = require('mysql');
+var Parser = require("body-parser");
+var app = express();
+
+app.use(Parser.urlencoded({extended: true}));
+// Will look for a file in local directory called "views" and for a file with ".ejs" at the end
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public")); // Use public folder to access css
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Noosa11',
+    database: 'quizbase'
+});
+
+// Check if server is working properly
+connection.connect(function(error) {
+    if(!!error) {
+        console.log("Error connecting to database");
+    } else {
+        console.log("Connected");
+    }
+});
+
+let signedInUser = { userID: "0", userName: "", loggedIn: false, currentDeckID: 0};
+
+app.get("/", function(req,res){
+  if(signedInUser.loggedIn){
+    res.redirect('/dashboard');
+  }
+  else{
+    res.render("homepage"); //the homepage of the ejs
+  }
+});
+
+app.get("/signin", function(req, res){
+  if(signedInUser.loggedIn){
+    res.redirect('/dashboard');
+  }
+  else{
+    res.render("signin"); //the homepage of the ejs
+  }
+  // res.render("signin");
+});
+
+app.post("/login", function(req, res){
+    let username = req.body.username;
+    let email = req.body.email;
+    console.log(username, email);
+    let q = "SELECT userId, username, email FROM user WHERE username = '" + username + "' AND email = '" + email + "'";
+    connection.query(q, function(err, results){
+      if(err) throw err;
+      if(results[0]){
+        console.log("The username and email are correct!");
+        signedInUser.userID = results[0].userId;
+        signedInUser.userName = results[0].username;
+        signedInUser.loggedIn = true;
+        res.redirect('/dashboard');
+      }
+      else{
+        console.log("The username or email is incorrect. Try again.");
+        res.redirect('/signin');
+      }
+    });
+});
+
+// app.get("/login", function(req, res){
+//   let username = req.query.username;
+//   let email = req.query.email;
+//   let q = "SELECT username, email FROM user WHERE username =" + username + " AND email = " + email;
+//   console.log(q, username, email);
+//   connection.query(q, function(err, results){
+//     if(err) throw err;
+//     if(results.username){
+//       res.redirect("/dashboard");
+//     }
+//   });
+// });
+
+// app.get("/homepage", function(req, res){
+//   res.render("homepage");
+// });
+
+app.post("/register", function(req, res){
+    let username = req.body.username;
+    let email = req.body.email;
+    console.log(username, email);
+    let q = "INSERT INTO user(username, email) VALUES ('" + username + "', '" + email + "')";
+    connection.query(q, function(err, results){
+      if(err) throw err;
+      console.log(results[0]);
+      // res.redirect("/dashboard");
+    });
+    q = "SELECT userId, username, email FROM user WHERE username = '" + username + "' AND email = '" + email + "'";
+    connection.query(q, function(err, results){
+      if(err) throw err;
+      if(results[0]){
+        console.log("Account Successfully Created!");
+        signedInUser.userID = results[0].userId;
+        signedInUser.userName = results[0].username;
+        signedInUser.loggedIn = true;
+        res.redirect('/dashboard');
+      }
+      else{
+        console.log("Account Unsuccessfully Created!");
+        res.redirect('/');
+      }
+    });
+});
+
+app.get("/dashboard", function(req, res){
+  console.log(signedInUser.userID);
+  if(signedInUser.loggedIn){
+    let result = [];
+    let q = "SELECT * FROM deck WHERE userId = " + signedInUser.userID;
+    connection.query(q, function(err, results){
+      if(err) throw err;
+      // console.log(results);
+        // res.render("dashboard", {key: results});
+        results.forEach(function(deck) {result.push(deck);})
+        // console.log(result[0].name);
+        res.render("dashboard", {key: result});
+    });
+  }
+  else{
+    res.redirect("/");
+  }
+});
+
+app.get("/showDeck", function(req, res){ //to edit the deck selected from dashboard
+  let deckID = req.query.edit;
+  if(deckID===undefined){
+    deckID = signedInUser.currentDeckID;
+  }
+  else{
+    signedInUser.currentDeckID = deckID;
+  }
+
+  let q = "SELECT name FROM deck WHERE deckId =" + deckID;
+  let r = "SELECT cardId, cardName, description FROM cards WHERE deckId = " + deckID;
+
+  let result = [];
+  connection.query(q, function(err, results){
+    if(err) throw err;
+    result.push(results[0].name);
+
+    connection.query(r, function(err, results){
+      if(err) throw err;
+      results.forEach(function(card) {result.push(card);})
+      console.log(result);
+
+      res.render("showDeck", {key: result, deckID: deckID});
+    });
+  });
+});
+
+app.post("/showDeck/editName", function(req, res){
+  let newName = req.body.deckName;
+  let deckID = req.body.edit;
+  // signedInUser.currentDeckID = deckID;
+  console.log(newName, deckID);
+  let q = "UPDATE deck SET name = '" + newName +"' WHERE deckId = " + deckID;
+  connection.query(q, function(err, results){
+    if(err) throw err;
+    console.log(results);
+    // res.redirect();
+  });
+  res.redirect("/showDeck");
+});
+
+app.post("/showDeck/updateCard", function(req, res){
+  let newCardName = req.body.cardName;
+  let newCardDescription = req.body.cardDescription;
+  let cardID = req.body.cardId;
+  let q = "UPDATE cards SET cardName = '" + newCardName +"' WHERE cardId = " + cardID;
+  connection.query(q, function(err, results){
+    if(err) throw err;
+    console.log(results);
+  });
+  q = "UPDATE cards SET description = '" + newCardDescription +"' WHERE cardId = " + cardID;
+  connection.query(q, function(err, results){
+    if(err) throw err;
+    console.log(results);
+  });
+  res.redirect("/showDeck");
+});
+
+app.post("/showDeck/deleteCard", function(req, res){
+  let cardID = req.body.delete;
+  let q = "DELETE FROM cards WHERE cardId = " + cardID;
+  connection.query(q, function(err, results){
+    if(err) throw err;
+  });
+  res.redirect("/showDeck");
+});
+
+app.get("/showDeck/addCard", function(req, res){
+  let deckID = signedInUser.currentDeckID;
+  console.log(deckID);
+  // let q = "SELECT cardId FROM cards ORDER BY cardId DESC LIMIT 1";
+  let q = "INSERT INTO cards(deckId, cardName, description) VALUES (" + deckID + ", 'Card Name Here', 'Description Here')";
+  console.log(q);
+  connection.query(q, function(err, results){
+    if(err) throw err;
+    // res.render("addCard", {key: result, deckID: deckID});
+    console.log(results);
+    res.redirect("/showDeck");
+  });
+
+});
+
+app.get("/displayDecks", function(req, res){
+  let topicID = req.query.box;
+  let q = "SELECT topicName FROM topic WHERE topicId = " + topicID;
+  console.log(topicID);
+  let d = "SELECT deckId, name FROM deck WHERE topicId = " + topicID;
+  // let result = {topicKey: topic};
+  let result=[];
+  connection.query(q, function(err, results){
+    if(err) throw err;
+    // console.log(results);
+    result.push(results[0].topicName);
+
+    connection.query(d, function(err, results){
+      if(err) throw err;
+      results.forEach(function(deck) {result.push(deck);}) //deck is object
+      // results.forEach(deck => {result.push(deck);})
+      console.log(result);
+      // result.push(results[0].name);
+      // console.log(results[0].name);
+      res.render("displayDecks", {key: result, name: "Connie"});
+    });
+  });
+  // let test = {key: result, name: "Connie"};
+  // res.render("displayDecks", {key: result, name: "Connie"});
+});
+
+app.get("/displayCards", function(req, res){
+  let deckID = req.query.deck;
+  let q = "SELECT name FROM deck WHERE deckId =" + deckID;
+  let r = "SELECT cardName, description FROM cards WHERE deckId = " + deckID;
+
+  let result = [];
+  connection.query(q, function(err, results){
+    if(err) throw err;
+    result.push(results[0].name);
+
+    connection.query(r, function(err, results){
+      if(err) throw err;
+      results.forEach(function(card) {result.push(card);})
+      console.log(result);
+
+      res.render("displayCards", {key: result});
+    });
+  });
+});
+
+app.post("/dashboard/deleteDeck", function(req, res){
+  let deckID = req.body.delete;
+  let q = "DELETE FROM cards WHERE deckId = " + deckID;
+  connection.query(q, function(err, results){
+    if(err) throw err;
+  });
+  q = "DELETE FROM deck where deckId = " + deckID;
+  connection.query(q, function(err, results){
+    if(err) throw err;
+  });
+  res.redirect("/dashboard");
+});
+
+
+// app.post("/register", function(req, res){
+//   let username = req.query.username;
+//   let email = req.query.email;
+//   let q = "INSERT INTO user(username, email) VALUES (" + username + "," + email + ")";
+//
+//   connection.query(q, function(err, results){
+//     // if(err) throw err;
+//     res.redirect("/dashboard");
+//   });
+// });
+
+
+//bottom
+app.listen(8080, function() {
+    console.log("Server running on 8080");
+});
---
+++ database.sql
@@ -0,0 +1,124 @@
+DROP DATABASE quizbase;
+CREATE DATABASE quizbase;
+
+USE quizbase;
+
+-- creating user
+CREATE TABLE school(
+  schoolId INT AUTO_INCREMENT PRIMARY KEY,
+  schoolName VARCHAR(255),
+  location VARCHAR(500)
+);
+
+CREATE TABLE topic(
+  topicId INT AUTO_INCREMENT PRIMARY KEY,
+  topicName VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE user(
+  userId INT AUTO_INCREMENT PRIMARY KEY,
+  username VARCHAR(255) NOT NULL UNIQUE,
+  email VARCHAR(255) NOT NULL UNIQUE
+  -- schoolId INT,
+  -- FOREIGN KEY(schoolId) REFERENCES school(schoolId)
+);
+
+CREATE TABLE deck(
+  deckId INT PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(255),
+  userId INT,
+  topicId INT,
+  creationDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  schoolId INT,
+  foreign key(userId) references user(userId),
+  foreign key(topicId) references topic(topicId),
+  foreign key(schoolId) references school(schoolId)
+);
+
+CREATE TABLE profile(
+  userId INT,
+  deckId INT,
+  FOREIGN KEY(userId) REFERENCES user(userId),
+  FOREIGN KEY(deckId) REFERENCES deck(deckId)
+);
+
+CREATE TABLE cards(
+  cardId INT PRIMARY KEY AUTO_INCREMENT,
+  deckId INT NOT NULL,
+  cardName VARCHAR(255),
+  description VARCHAR(2000),
+  FOREIGN KEY(deckId) REFERENCES deck(deckId)
+);
+
+CREATE TABLE class(
+  classId INT PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(255),
+  topicId INT,
+  description VARCHAR(600),
+  foreign key(topicId) references topic(topicId)
+);
+
+CREATE TABLE members(
+  userId INT,
+  classId INT,
+  foreign key(userId) references user(userId),
+  foreign key(classId) references class(classId)
+);
+
+
+INSERT INTO user(username, email)
+VALUES ("Connie", "connie@gmail.com"),
+  ("Friend", "Friend@gmail.com"),
+  ("Cindy", "Cindy@gmail.com");
+
+INSERT INTO school(schoolName, location)
+VALUES ("City College", "Manhattan"),
+  ("Hunter", "Manhattan"),
+  ("College of Staten Island", "Staten Island"),
+  ("Brooklyn College", "Brooklyn");
+
+INSERT INTO topic(topicName)
+VALUES ("Math"),
+  ("Science"),
+  ("Computer Science"),
+  ("English"),
+  ("Art"),
+  ("History"),
+  ("Music");
+
+INSERT INTO deck(name, userId, topicId, creationDate, schoolId)
+VALUES ("Linear Algebra", 1, 1, CURDATE(), 2),
+  ("Biology", 2, 2, DATE '2017-12-17', 1),
+  ("Mozart", 2, 7, DATE '2018-08-29', 1),
+  ("Chemistry", 3, 2, DATE '2016-12-17', 2),
+  ("Algortihms", 1, 3, DATE '2017-05-15', 1),
+  ("Shading", 1, 5, DATE '2017-12-01', 1),
+  ("Literacy Techniques", 3, 4, DATE '2017-01-17', 2),
+  ("American Revolution", 1, 6, DATE '2018-01-17', 1),
+  ("Constitution", 1, 6, DATE '2018-05-17', 3),
+  ("Data Structures", 3, 3, DATE '2018-8-30', 3);
+
+INSERT INTO cards(deckId, cardName, description)
+VALUES (1, "Matrix", "asdfghjkl"),
+  (1, "Eigen Value", "asdfghjkl"),
+  (1, "Identity Matrix", "asdfghjkl");
+
+INSERT INTO cards(deckId, cardName, description)
+VALUES (3, "Queue", "asdfghjkl"),
+  (3, "Stack", "asdfghjkl"),
+  (3, "Binary Tree", "asdfghjkl");
+
+INSERT INTO cards(deckId, cardName, description)
+VALUES (2, "Chromosomes", "asdfghjkl"),
+  (2, "Cell", "asdfghjkl"),
+  (2, "Mitochondria", "asdfghjkl");
+
+INSERT INTO class(name, topicId, description)
+VALUES ("CSc 336", 3, "asdfghjkl"),
+  ("Physics 1", 2, "asdfghjkl"),
+  ("Biology", 2, "asdfghjkl");
+
+INSERT INTO members(userId, classId)
+VALUES (1, 1),
+  (2, 3),
+  (3, 2);
