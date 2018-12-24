4dd493e3fb18307c46c37f9f6f65e93b135aa8fe
connie-code <conniewu06@gmail.com>
2018-12-23 22:30:46 -0500

    Manually merged touch_dropdown_menu branch to master + resolved conflicts + deleted uneccessary code that made the website not as intended

--- views/homepage.js
+++
@@ -1,7 +0,0 @@
-// $('.message a').click(function(){
-//   $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
-// });
-
-// function getvalue(){
-//   document.getElementByName("email").value = document.getElementById()
-// }
